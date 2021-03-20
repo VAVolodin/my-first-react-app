9a17003b75995c310f4d70742374c176f3ffb609
Vladimir00505 <vladimir00505@gmail.com>
2021-03-20 22:41:35 +0300

    lesson2 update

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ClassComponent from './ClassComponent';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{new Date().toLocaleTimeString()}</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <ClassComponent dick={20} />
+            <ClassComponent ass="vtorye propersy" />
+        </>
+    );
 }
-setInterval(App, 1000);
 
 export default App;
--- src/ClassComponent.tsx
+++ src/ClassComponent.tsx
@@ -1,16 +1,52 @@
 import React from 'react';
+import style from './ClassComponent.module.scss';
 
 interface IProps {
-    data: string;
+    ass?: string; // data?: необязательный к вызову параметр
+    dick?: number;
 }
-class ClassComponent extends React.Component<IProps>{
-    constructor (props: IProps) {
+
+interface IState {
+    click: number;
+    name: string;
+}
+class ClassComponent extends React.Component<IProps, IState> {
+    constructor(props: IProps) {
         super(props);
-        this.state = {};
+        this.state = { click: 0, name: 'Жопа!' };
+        // необходимо указать this.handleClickSub непосредственную внешнюю область представления, т.к. стрелочная функция handleClickAdd не имеет своего .this то берет его из внешнего окружения, т.е. у ClassComponent, для handleClickSub объявленной как funcion decaration метод .this обращается к самой вызывающей функции  далее к конструктору, что выдает ошибку this"undefined"
+        this.handleClickSub = this.handleClickSub.bind(this);
+    }
+
+    handleClickAdd = () => {
+        const { click } = this.state;
+        this.setState({ click: click + 1 });
+    };
+
+    handleClickSub() {
+        const { click } = this.state;
+        this.setState({ click: click - 1 });
     }
 
     render() {
-        return <h1>Hi motherFuckers, I'm a ClassComponent!!! =) </h1>;
+        const { ass, dick } = this.props;
+        const { click, name } = this.state;
+        return (
+            <>
+                <h1>Hi { ass } { dick }, I&apos;m a ClassComponent!!! =) </h1>
+                <button type="button" onClick={this.handleClickAdd}>
+                    push me to add +
+                </button>
+                <button
+                    type="button"
+                    className={style.wrapper}
+                    onClick={this.handleClickSub}>
+                    touch me to sub -1
+                </button>
+                <h2>{click}</h2>
+                <h2>{name}</h2>
+            </>
+        );
     }
 }
 
